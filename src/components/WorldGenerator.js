8b76a785fa7e7a46d19c503b1b16a22574f80a09
matthewmoodley048 <67504755+matthewmoodley048@users.noreply.github.com>
2023-07-16 19:46:43 +0200

    fix(WorldGenerator): fixed textures not rendering on blocks

--- src/components/WorldGenerator.js
+++ src/components/WorldGenerator.js
@@ -1,12 +1,10 @@
-import { grassTexture } from "../images/textures" 
+import { dirtTexture, grassTexture } from "../images/textures" 
 import { Cube } from "./Cube"
-export const WorldGenerator = ({position, texture}) => {
-  const activeTexture =grassTexture
-  
+export const WorldGenerator = ({}) => {
   return(
      <>
         {Array(10).fill().map((_, index) => (
-              <Cube position={[index,index,index]} texture={activeTexture}/>
+              <Cube position={[index,index,index]} texture={'grass'}/>
         ))}
      </>
     )
